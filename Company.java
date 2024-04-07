eaf587a7441dab4e32415ce038665056c8a0c6a1
Mateusz <mateuszryngiewicz1@gmail.com>
2024-04-07 18:50:55 +0200

    Zad3

--- Company.java
+++ Company.java
@@ -6,17 +6,28 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        // Dodawanie pracowników
+        
         employees[0] = new Manager("Mateusz Ryngiewicz", 10000, 0);
         employees[1] = new Worker("Szymon Ryngiewicz", 120000, "Deweloper");
         employees[2] = new Employee("Małgoarzata Ryngiewicz", 14000);
         employees[3] = new Worker("Maciek Aulich", 7000, "Księgowy");
         employees[4] = new Worker("Nikola Pająk", 20000, "Menedżer marketingu");
 
-        // Wyświetlanie danych dla wszystkich pracowników
-        System.out.println("Dane dla wszystkich pracowników:");
-        for (Employee employee : employees) {
-            System.out.println(employee);
-        }
-    }
-}
+        
+        
+         int nonManagerCount = 0;
+         for (Employee employee : employees) {
+             if (!(employee instanceof Manager)) {
+                 nonManagerCount++;
+             }
+         }
+         ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
+         employees[0].setSalary(17000);
+ 
+         
+         System.out.println("Dane dla wszystkich pracowników:");
+         for (Employee employee : employees) {
+             System.out.println(employee);
+         }
+     }
+ }
