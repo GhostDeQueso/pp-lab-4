5d7868f6bb72c40e12877724a02ff18f7ddbf1c8
Mateusz <mateuszryngiewicz1@gmail.com>
2024-04-07 18:42:03 +0200

    Zad2

--- Company.java
+++ Company.java
@@ -1,20 +1,20 @@
+import employees.Employee;
+import employees.Manager;
+import employees.Worker;
+
 public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Employee("Mateusz Ryngiewicz", 10000);
-        employees[1] = new Employee("Szymon Ryngiewicz", 120000);
+        // Dodawanie pracowników
+        employees[0] = new Manager("Mateusz Ryngiewicz", 10000, 0);
+        employees[1] = new Worker("Szymon Ryngiewicz", 120000, "Deweloper");
         employees[2] = new Employee("Małgoarzata Ryngiewicz", 14000);
-        employees[3] = new Employee("Maciek Aulich", 7000);
-        employees[4] = new Employee("Nikola Pająk", 20000);
+        employees[3] = new Worker("Maciek Aulich", 7000, "Księgowy");
+        employees[4] = new Worker("Nikola Pająk", 20000, "Menedżer marketingu");
 
-        System.out.println("Dane dla pracownika o indeksie 3:");
-        System.out.println(employees[3]);
-
-        employees[3].setSalary(10000);
-
-        
-        System.out.println("\nDane dla wszystkich pracowników:");
+        // Wyświetlanie danych dla wszystkich pracowników
+        System.out.println("Dane dla wszystkich pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
         }
